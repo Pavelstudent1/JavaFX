8e1738bfc47243b6c4bff30cf30899121d152bdc
unknown <pvfedorov6861@gmail.com>
2016-07-06 00:55:27 +0400

    current

--- src/ui_probe_v3/UI_Example.java
+++ src/ui_probe_v3/UI_Example.java
@@ -1,12 +1,23 @@
 package ui_probe_v3;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Accordion;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TitledPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class UI_Example extends Application {
@@ -36,11 +47,17 @@ public class UI_Example extends Application {
 		
 		TabPane tabs = new TabPane();
 		tabs.prefWidthProperty().bind(primaryStage.widthProperty());
+		tabs.prefHeightProperty().bind(primaryStage.heightProperty());
 		
 		Tab tab1 = new Tab("Main");
 		tab1.setContent(mainPane);
-		Tab tab2 = new Tab("Log");
+		Tab tab2 = new Tab("Settings");
 		tabs.getTabs().addAll(tab1, tab2);
+		
+		/**
+		 * http://code.makery.ch/library/javafx-8-tutorial/ru/part4/
+		 */
+		testFillingTab2(tab2, tabs);
 		
 		root.getChildren().add(tabs);
 		
@@ -49,6 +66,29 @@ public class UI_Example extends Application {
 		primaryStage.setWidth(DEF_WINDOW_WIDTH);
 		primaryStage.setHeight(DEF_WINDOW_HEIGHT);
 		primaryStage.show();
+	}
+
+	private void testFillingTab2(Tab tab2, TabPane tabs) {
+
+		Pane settingPane = new FlowPane();
+		tab2.setContent(settingPane);
+		
+		TitledPane s1 = new TitledPane("Setting 1", new Text("SETTING 1"));
+		TitledPane s2 = new TitledPane("Setting 2", new Text("SETTING 2"));
+		TitledPane s3 = new TitledPane("Setting 3", new Text("SETTING 3"));
+		
+		Accordion accordion = new Accordion();
+		accordion.prefWidthProperty().bind(tabs.prefWidthProperty());
+//		accordion.prefHeightProperty().bind(tabs.prefHeightProperty());
+		accordion.getPanes().addAll(s1, s2, s3);
+		
+		HBox s1hbox = new HBox(new ColorPicker(Color.GREEN), new ColorPicker(Color.RED));
+		s1hbox.setPadding(new Insets(5,5,5,5));
+		HBox.setMargin(s1hbox.getChildren().get(0), new Insets(0, 10, 0, 10));
+		HBox.setMargin(s1hbox.getChildren().get(1), new Insets(0, 10, 0, 10));
+		s1.setContent(s1hbox);
+		
+		settingPane.getChildren().add(accordion);
 	}
 
 	private FlowPane createMainPane(Stage primaryStage) {
